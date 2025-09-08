200e111def455d0e7d05d610e3168773d37b31fd
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-08 19:36:04 +0100

    Delete 040925_ClassWork_2.py

--- 040925_ClassWork_2.py
+++
@@ -1,9 +0,0 @@
-#Author Vira Ustymenko
-#Date: 04 September 2025
-#Description: Program asks the user for a number under 20, if it is too high it informs the user
-
-user_number = int(input("Please enter a number under 20: "))
-if user_number>=20:
-    print("Too high.")
-else:
-    print("Thank you.")
