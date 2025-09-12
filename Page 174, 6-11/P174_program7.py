ab55980a84e8d20d6a7ae0410cd1900fb957b82b
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:40:44 +0100

    Update P174_program7.py

--- Page 174, 6-11/P174_program7.py
+++ Page 174, 6-11/P174_program7.py
@@ -2,10 +2,11 @@
 #Date: 02 September 2025
 #Description: Program accepts marks in 5 subjects and outputs the average
 
-SubjOne = int(input("Enter the first grade: "))
-SubjTwo = int(input("Enter the second grade: "))
-SubjThree = int(input("Enter the third grade: "))
-SubjFour = int(input("Enter the fourth grade: "))
-SubjFive = int(input("Enter the fifth grade: "))
+SubjOne = float(input("Enter the first grade: "))
+SubjTwo = float(input("Enter the second grade: "))
+SubjThree = float(input("Enter the third grade: "))
+SubjFour = float(input("Enter the fourth grade: "))
+SubjFive = float(input("Enter the fifth grade: "))
 Average = (SubjOne + SubjTwo + SubjThree + SubjFour + SubjFive)/5
 print("Average grade is: ", Average)
+
