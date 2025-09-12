66466668110584f33bfd72e05a0d086b75a81aa8
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:37:54 +0100

    Update Assignment_ViraUstymenko_4.py

--- Programming Assignment 1/Assignment_ViraUstymenko_4.py
+++ Programming Assignment 1/Assignment_ViraUstymenko_4.py
@@ -3,11 +3,12 @@
 #Description: Program requests three numbers as input and displays the sum of the first two multiplied by the third num.
 
 number_1 = input("Enter the first number: ")
-number_1 = int(number_1)
+number_1 = float(number_1)
 number_2 = input("Enter the second number: ")
-number_2 = int(number_2)
+number_2 = float(number_2)
 number_3 = input("Enter the third number: ")
-number_3 = int(number_3)
+number_3 = float(number_3)
 total = number_1 + number_2
 answer = total * number_3
 print("The answer is:", answer)
+
