43bbca44b140e77184e30a579c585378ceefcae9
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:41:33 +0100

    Update P174_program9.py

--- Page 174, 6-11/P174_program9.py
+++ Page 174, 6-11/P174_program9.py
@@ -2,8 +2,9 @@
 #Date: 02 September 2025
 #Description: Program asks for number n and outputs n squared, n cubed, and n to the power of 4
 
-n = int(input("Enter number n: "))
+n = float(input("Enter number n: "))
 n_2 = n * n
 n_3 = n_2 * n
 n_4 = n_3 * n
 print("n squared =", n_2, "	n cubed =", n_3, "	n to the power of 4 =", n_4)
+
