b69f01d11f905a8adb4ff17962b3497248e9311e
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:43:38 +0100

    Update 040925_ClassWork_2.py

--- If, elif, else/040925_ClassWork_2.py
+++ If, elif, else/040925_ClassWork_2.py
@@ -2,8 +2,9 @@
 #Date: 04 September 2025
 #Description: Program asks the user for a number under 20, if it is too high it informs the user
 
-user_number = int(input("Please enter a number under 20: "))
+user_number = float(input("Please enter a number under 20: "))
 if user_number>=20:
     print("Too high.")
 else:
+
     print("Thank you.")
