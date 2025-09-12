bb7a96caa06c630006ba84493d40d5288dd32f8e
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:39:31 +0100

    Update Assignment_ViraUstymenko_5.py

--- Programming Assignment 1/Assignment_ViraUstymenko_5.py
+++ Programming Assignment 1/Assignment_ViraUstymenko_5.py
@@ -1,7 +1,8 @@
 #Author: Vira Ustymenko
 #Date: 01 September 2025
 #Description: Program requests the starting num. of pizza slices and the eaten num. of them, then displays what is left over.
-startPizza = int(input("Enter the number of pizza slices you started with: "))
-eatenPizza = int(input("Enter the number of pizza slices you have eaten: "))
+startPizza = float(input("Enter the number of pizza slices you started with: "))
+eatenPizza = float(input("Enter the number of pizza slices you have eaten: "))
 leftOver = startPizza - eatenPizza
 print("There is", leftOver, "pizza slices left over.")
+
