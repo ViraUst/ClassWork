124702218f3415498f8745cc0c3f1b51c121f8d9
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:42:09 +0100

    Update P174_program10.py

--- Page 174, 6-11/P174_program10.py
+++ Page 174, 6-11/P174_program10.py
@@ -2,7 +2,8 @@
 #Date: 02 September 2025
 #Description: Program finds area of a triangle
 
-base = int(input("Enter base of the triangle: "))
-height = int(input("Enter height of the triangle: "))
+base = float(input("Enter base of the triangle: "))
+height = float(input("Enter height of the triangle: "))
 area = 0.5 * base * height
 print("Area =", area)
+
