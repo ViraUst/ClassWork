b546d33e92ac160c58fbb458debb73fd6c33adda
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:44:14 +0100

    Update 040925_ClassWork_3.py

--- If, elif, else/040925_ClassWork_3.py
+++ If, elif, else/040925_ClassWork_3.py
@@ -2,8 +2,9 @@
 #Date: 04 September 2025
 #Description: Program asks the user for a number between 10 and 20 inclusive, if the number is higher or lower it outputs "incorrect value"
 
-user_number = int(input("Please enter a number between 10 and 20 inclusive: "))
+user_number = float(input("Please enter a number between 10 and 20 inclusive: "))
 if user_number>=10 and user_number<=20:
     print("Thank you.")
 else:
+
     print("incorrect value entered.")
