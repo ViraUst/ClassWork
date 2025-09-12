d69460bf3c6e4e49266d2077c70891ddbe797189
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-12 09:37:19 +0100

    Update Assignment_ViraUstymenko_3.py

--- Programming Assignment 1/Assignment_ViraUstymenko_3.py
+++ Programming Assignment 1/Assignment_ViraUstymenko_3.py
@@ -3,8 +3,9 @@
 #Description: Program requests two numbers as input and displays their sum
 
 number_1 = input("Enter the first number: ")
-number_1 = int(number_1)
+number_1 = float(number_1)
 number_2 = input("Enter the second number: ")
-number_2 = int(number_2)
+number_2 = float(number_2)
 total = number_1 + number_2
 print("Sum =", total)
+
