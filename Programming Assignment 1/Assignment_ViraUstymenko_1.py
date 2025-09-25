806913bd35a7f8736589e723f01d4ed55f0d3ec5
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-09-25 09:42:12 +0100

    Update Assignment_ViraUstymenko_1.py

--- Programming Assignment 1/Assignment_ViraUstymenko_1.py
+++ Programming Assignment 1/Assignment_ViraUstymenko_1.py
@@ -1,6 +1,7 @@
 #Author: Vira Ustymenko
 #Date: 01 September 2025
-#Desc: Program to request user name as input and returns name with a greeting
+#Desc: Program requests user name as input and returns name with a greeting
 name = input("Please enter your name: ")
 print("Hello, ", name)
 #Edit: deleted 'surname in the line above'
+
