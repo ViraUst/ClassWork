f8c96ed24c39f9bdba08aad21e5a3e1b586aaf16
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-12-09 13:45:37 +0000

    Update accumulator7.py

--- Accumulator091225/accumulator7.py
+++ Accumulator091225/accumulator7.py
@@ -1,5 +1,5 @@
 #Author: Vira Ustymenko
-#Date: 12th December 2025
+#Date: 8th December 2025
 #Description: Program outputs product of the even numbers
 # between 1 and 50
 
@@ -8,4 +8,5 @@ integ = 1
 for i in range(1,51):
     if i%2==0:
         integ = integ*i
+
 print(integ)
