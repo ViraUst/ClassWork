c8727aa20a6ec904d2c15a7c75672aa9c7ceb028
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-12-09 13:44:40 +0000

    Update accumulator1.py

--- Accumulator091225/accumulator1.py
+++ Accumulator091225/accumulator1.py
@@ -1,8 +1,9 @@
 #Author: Vira Ustymenko
-#Date: 12th December 2025
+#Date: 8th December 2025
 #Description: Program sums the first 50 integers
 
 integer = 0
 for i in range(1,51):
     integer = integer + i
+
 print(integer)
