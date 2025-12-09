4674d66ad424933ac12846ad8f047ce798bf8779
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-12-09 13:44:50 +0000

    Update accumulator2.py

--- Accumulator091225/accumulator2.py
+++ Accumulator091225/accumulator2.py
@@ -1,5 +1,5 @@
 #Author: Vira Ustymenko
-#Date: 12th December 2025
+#Date: 8th December 2025
 #Description: Program sums the first n integers entered by
 # the user
 
@@ -8,4 +8,5 @@ many = int(input("Enter a number of integers to be summed: "))
 
 for i in range(1,(many+1)):
     integer = integer + i
+
 print(integer)
