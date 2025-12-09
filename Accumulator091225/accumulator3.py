20c5404fa6c07e989c741ed26ed4520f498d219b
ViraUst <22VUstymenko.acc@lwetb.ie>
2025-12-09 13:45:01 +0000

    Update accumulator3.py

--- Accumulator091225/accumulator3.py
+++ Accumulator091225/accumulator3.py
@@ -1,9 +1,10 @@
 #Author: Vira Ustymenko
-#Date: 12th December 2025
+#Date: 8th December 2025
 #Description: Program outputs product of the first 5 int
 
 integ = 1
 
 for i in range(1,6):
     integ = integ*i
+
 print(integ)
