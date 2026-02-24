db04b2f74840687f7c789cdfd1f2ea857b947dc9
ViraUst <22VUstymenko.acc@lwetb.ie>
2026-02-24 18:43:37 +0000

    Add files via upload

---
+++ print formatting, math module and reference revision/Ex1.py
@@ -0,0 +1,29 @@
+#Author: Vira Ustymenko
+#Date: 23rd Februrary 2026
+#Description: Exercise 1
+
+import math
+
+#(i)
+a=2
+b=3
+c=6
+print(math.sqrt(math.pow(a,2) + math.pow(b,2) + math.pow(c,2)))
+
+#(ii)
+y = 1
+print(2-y*math.pow(math.e,2*y)+4*y)
+
+#(iii)
+p =1
+q =1
+r =1
+s =0
+print(p+q/(math.pow(r+s,4)))
+
+#(iv)
+x = 2
+print((math.cos(x)/math.tan(x))+x)
+
+#(v)
+print(math.fabs(math.pow(math.e,2)-x))
---
+++ print formatting, math module and reference revision/Ex2.py
@@ -0,0 +1,11 @@
+#Author: Vira Ustymenko
+#Date: 23rd Februrary 2026
+#Description: Exercise 2, calculate area and volume of a sphere
+import math
+
+r = 7.5
+A = 4*math.pi*math.pow(r,2)
+print("Area of the sphere: ", A)
+
+V = (4/3)*math.pi*math.pow(r,3)
+print("Volume of the sphere: ", V)
